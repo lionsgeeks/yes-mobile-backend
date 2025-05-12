de7bdd1c709994be89eee6590208af1de9d8c49d
Ayman Boujjar <aymenboujjar12@gmail.com>
2025-05-12 15:52:54 +0100

    Merge branch 'main' of https://github.com/lionsgeeks/yes-mobile-backend

---
+++ resources/views/maizzlMails/invitation.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en" xmlns:v="urn:schemas-microsoft-com:vml">
+<head>
+  <meta charset="utf-8">
+  <meta name="x-apple-disable-message-reformatting">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="format-detection" content="telephone=no, date=no, address=no, email=no, url=no">
+  <meta name="color-scheme" content="light dark">
+  <meta name="supported-color-schemes" content="light dark">
+  <!--[if mso]>
+  <noscript>
+    <xml>
+      <o:OfficeDocumentSettings xmlns:o="urn:schemas-microsoft-com:office:office">
+        <o:PixelsPerInch>96</o:PixelsPerInch>
+      </o:OfficeDocumentSettings>
+    </xml>
+  </noscript>
+  <style>
+    td,th,div,p,a,h1,h2,h3,h4,h5,h6 {font-family: "Segoe UI", sans-serif; mso-line-height-rule: exactly;}
+    .mso-break-all {word-break: break-all;}
+  </style>
+  <![endif]-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet" media="screen">
+  <style>
+    @media (max-width: 600px) {
+      .sm-p-6 {
+        padding: 24px !important
+      }
+    }
+  </style>
+</head>
+<body style="margin: 0; width: 100%; padding: 0; -webkit-font-smoothing: antialiased; word-break: break-word">
+  <div role="article" aria-roledescription="email" aria-label lang="en">
+    <div style="margin-left: auto; margin-right: auto; width: 100%; max-width: 552px">
+      <div role="separator" style="line-height: 24px">&zwj;</div>
+      <table style="width: 100%" cellpadding="0" cellspacing="0" role="none">
+        <tr>
+          <td class="sm-p-6" style="border-radius: 8px; background-color: #fffffe; padding: 24px 36px; border: 1px solid #e2e8f0">
+            <a href="https://maizzle.com">
+              <img src="images/yeslogo.png" width="70" alt="Maizzle" style="max-width: 100%; vertical-align: middle">
+            </a>
+            <div role="separator" style="line-height: 24px">&zwj;</div>
+            <h1 style="margin: 0 0 24px; font-size: 24px; line-height: 32px; font-weight: 600; color: #0f172a">
+              Hello Bojo welcome to YES AFRICA!
+            </h1>
+            <p style="margin: 0 0 24px; font-size: 16px; line-height: 24px; color: #475569">
+              We're happy to have you on board! This is your email address and your password in order to activate your account:
+            </p>
+            <p style="margin: 0 0 24px; font-size: 16px; line-height: 24px; color: #475569">
+              <strong>Email:</strong> <span style="color: #0f172a">admin@gmail.com</span>
+              <br>
+              <strong>Password:</strong> <span style="color: #0f172a">password</span>
+            </p>
+            <div role="separator" style="line-height: 24px">&zwj;</div>
+            <p style="margin: 0; font-size: 16px; line-height: 24px; color: #475569">
+              Thanks,
+              <br>
+              <span style="font-weight: 600">YES AFRICA</span>
+            </p>
+            <div role="separator" style="height: 1px; line-height: 1px; background-color: #cbd5e1; margin-top: 24px; margin-bottom: 24px">&zwj;</div>
+            <p class="mso-break-all" style="margin: 0; font-size: 12px; line-height: 20px; color: #475569">
+              If you're having trouble acces to your app Contact us at youthempowermentsummit@gmail.com
+              <br>
+            </p>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</body>
+</html>
