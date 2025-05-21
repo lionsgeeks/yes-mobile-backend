4fae568de3850e2d446159ec96aa6bf7ec8b3586
osama-jeb <oussamajebrane98@gmail.com>
2025-05-21 16:44:35 +0100

    program controller dd

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -10,7 +10,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 
 class ParticipantController extends Controller
