4e1dd6f78bd4f9a50bd773bf1eb70170ebc46e0f
osama-jeb <oussamajebrane98@gmail.com>
2025-06-13 16:22:36 +0100

    excel removed style

--- app/Exports/ParticipantsExport.php
+++ app/Exports/ParticipantsExport.php
@@ -7,12 +7,8 @@ use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
-use Maatwebsite\Excel\Concerns\WithStyles;
-use PhpOffice\PhpSpreadsheet\Style\Color;
-use PhpOffice\PhpSpreadsheet\Style\Fill;
-use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class ParticipantsExport implements FromQuery, WithHeadings, WithMapping, ShouldAutoSize, WithStyles
+class ParticipantsExport implements FromQuery, WithHeadings, WithMapping, ShouldAutoSize
 {
 
     public $role;
@@ -60,42 +56,10 @@ class ParticipantsExport implements FromQuery, WithHeadings, WithMapping, Should
             $participant->description,
             $participant->created_at,
             $participant->updated_at,
-            $participant->social->website,
-            $participant->social->linkedin,
-            $participant->social->youtube,
-            $participant->social->instagram,
-        ];
-    }
-
-    public function styles(Worksheet $sheet)
-    {
-        $highestRow = $sheet->getHighestRow();
-
-        for ($row = 2; $row <= $highestRow; $row++) {
-            $color = $row % 2 == 0 ? 'FFFFFF' : 'E8E8E8';
-
-            $sheet->getStyle("A{$row}:Z{$row}")
-                ->applyFromArray([
-                    'fill' => [
-                        'fillType'   => Fill::FILL_SOLID,
-                        'startColor' => ['argb' => $color],
-                    ],
-                ]);
-        }
-
-        return [
-            1    => [
-                'font' => [
-                    'bold' => true,
-                    'color' => ['argb' => 'FFFFFF'],
-
-                ],
-                'fill' => [
-                    'fillType'   => Fill::FILL_SOLID,
-                    'startColor' => ['argb' => Color::COLOR_ALPHA],
-                ],
-
-            ],
+            $participant->social?->website,
+            $participant->social?->linkedin,
+            $participant->social?->youtube,
+            $participant->social?->instagram,
         ];
     }
 }
